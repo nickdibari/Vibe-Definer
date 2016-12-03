446fb319f45a8a5fdcff6f1915fbd87693639640
nickdibari <ndibari@fordham.edu>
2016-12-03 14:25:27 -0500

    Got lyrics and song information from song chart

--- VibeDefiner.py
+++ VibeDefiner.py
@@ -6,6 +6,7 @@
 # Python script to generate mood of song from lyrics
 
 from keys import MUSIXMATCH_KEY
+from models import Song
 
 import requests
 
@@ -18,6 +19,7 @@ MUSIX_BASE = 'http://api.musixmatch.com/ws/1.1/'
 
 def GetLyrics():
     song_IDs = []
+    songs = {}
 
     # Get list of songs
     chart_url = '{0}chart.tracks.get?apikey={1}&page=1&page_size=10&\
@@ -32,13 +34,44 @@ def GetLyrics():
     track_list = chart_resp_json['body']['track_list']
 
     for i in range(len(track_list)):
-        print('{0} has code of:'.format(track_list[i]['track']['track_name']))
-        print(track_list[i]['track']['track_id'])
         song_IDs.append(track_list[i]['track']['track_id'])
 
+    for code in song_IDs:
+        # Get lyrics from songs
+        lyrics_url = '{0}track.lyrics.get?apikey={1}&track_id={2}'\
+        .format(MUSIX_BASE, MUSIXMATCH_KEY, song_IDs[2])
+        
+        lyrics_response = requests.get(lyrics_url)
+        lyrics_resp_json = lyrics_response.json()['message']
+        
+        if lyrics_resp_json['header']['status_code'] == 200:
+            print('Got lyrics of song {0} OK'.format(code))
+        
+        lyrics = lyrics_resp_json['body']['lyrics']['lyrics_body']
 
+        # Get info from songs
+        info_url = '{0}track.get?apikey={1}&track_id={2}'\
+        .format(MUSIX_BASE, MUSIXMATCH_KEY, song_IDs[2])
+
+        info_response = requests.get(info_url)
+        info_resp_json = info_response.json()['message']
+
+        if info_resp_json['header']['status_code'] == 200:
+            print('Got song info of song {0} OK'.format(code))
+
+        artist = info_resp_json['body']['track']['artist_name']
+        track_name = info_resp_json['body']['track']['track_name']
+        
+        # Package song 
+        song = Song(artist, track_name, lyrics)
+        songs[code] = song
+
+    return songs
+    
+    
 def main():
-    GetLyrics()
+    songs = GetLyrics()
+
 
 
 if __name__ == '__main__':
