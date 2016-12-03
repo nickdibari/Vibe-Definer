c01bd6d5931a33e210f3c92d78e4897da71397bd
nickdibari <ndibari@fordham.edu>
2016-12-03 17:40:52 -0500

    PEP8 Compliance models.py

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 # Class Definitions for VibeDefiner
 
 # Song Object
+
+
 class Song(object):
-	"""Object to hold Song artist/track name/lyrics"""
-	def __init__(self, artist, track_name, lyrics):
-		self.artist = artist
-		self.name = track_name
-		self.lyrics = lyrics
-		
-		
+    """Object to hold Song artist/track name/lyrics"""
+    def __init__(self, artist, track_name, lyrics):
+        self.artist = artist
+        self.name = track_name
+        self.lyrics = lyrics
