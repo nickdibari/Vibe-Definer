a9f1103af94bef7123d50e290e8134918b022ccb
nickdibari <ndibari@fordham.edu>
2016-12-03 14:14:52 -0500

    Removed genre attribute from Song class

--- models.py
+++ models.py
@@ -3,10 +3,9 @@
 # Song Object
 class Song(object):
 	"""Object to hold Song lyrics/artist/name/genre"""
-	def __init__(self, artist, track_name, genre, lyrics):
+	def __init__(self, artist, track_name, lyrics):
 		self.artist = artist
 		self.name = track_name
-		self.genre = genre
 		self.lyrics = lyrics
 		
 		
