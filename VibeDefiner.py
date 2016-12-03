aa314b2be389c50c6f1cb9a1bd23e65d136e9ead
nickdibari <ndibari@fordham.edu>
2016-12-03 12:55:07 -0500

    Made PEP8 Compliant

--- VibeDefiner.py
+++ VibeDefiner.py
@@ -8,34 +8,38 @@
 from keys import MUSIXMATCH_KEY
 
 import requests
-from pprint import pprint
 
 # Base API URL for Musixmatch API
 MUSIX_BASE = 'http://api.musixmatch.com/ws/1.1/'
 
 # GetLyrics
 # Returns list of lyrics for analysis
+
+
 def GetLyrics():
-	song_IDs = []
+    song_IDs = []
 
-	# Get list of songs 
-	chart_url = '{0}chart.tracks.get?apikey={1}&page=1&page_size=10&f_has_lyrics=1'.format(MUSIX_BASE, MUSIXMATCH_KEY) 
-	
-	chart_response = requests.get(chart_url)
-	chart_resp_json = chart_response.json()['message']
+    # Get list of songs
+    chart_url = '{0}chart.tracks.get?apikey={1}&page=1&page_size=10&\
+    f_has_lyrics=1'.format(MUSIX_BASE, MUSIXMATCH_KEY)
 
-	if chart_resp_json['header']['status_code'] == 200:
-		print('Got JSON of charts OK')
+    chart_response = requests.get(chart_url)
+    chart_resp_json = chart_response.json()['message']
 
-	track_list = chart_resp_json['body']['track_list']
-	
-	for i in range(len(track_list)):
-		print('{0} has code of:'.format(track_list[i]['track']['track_name']))
-		print(track_list[i]['track']['track_id'])
-		song_IDs.append(track_list[i]['track']['track_id'])
+    if chart_resp_json['header']['status_code'] == 200:
+        print('Got JSON of charts OK')
+
+    track_list = chart_resp_json['body']['track_list']
+
+    for i in range(len(track_list)):
+        print('{0} has code of:'.format(track_list[i]['track']['track_name']))
+        print(track_list[i]['track']['track_id'])
+        song_IDs.append(track_list[i]['track']['track_id'])
+
 
 def main():
-	GetLyrics()
+    GetLyrics()
+
 
 if __name__ == '__main__':
-	main()
+    main()
