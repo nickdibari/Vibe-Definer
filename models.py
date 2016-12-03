1f31b8b2e429ac2c971417655c2ec678908d6c0f
nickdibari <ndibari@fordham.edu>
2016-12-03 14:34:58 -0500

    Updated doctring for Song class

--- models.py
+++ models.py
@@ -2,7 +2,7 @@
 
 # Song Object
 class Song(object):
-	"""Object to hold Song lyrics/artist/name/genre"""
+	"""Object to hold Song artist/track name/lyrics"""
 	def __init__(self, artist, track_name, lyrics):
 		self.artist = artist
 		self.name = track_name
