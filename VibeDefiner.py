0c00168643cb833074615d166a99384c69a2e6be
nickdibari <ndibari@fordham.edu>
2016-12-03 21:00:30 -0500

    Implementation of artist search feature

--- VibeDefiner.py
+++ VibeDefiner.py
@@ -10,67 +10,59 @@ from models import Song
 
 import requests
 import webbrowser
+import json
 
 # Base API URLs 
-MUSIX_BASE = 'http://api.musixmatch.com/ws/1.1/'
+MUSIX_BASE = 'http://api.musixmatch.com/ws/1.1'
 TEXT_ANALYTICS_BASE = 'https://westus.api.cognitive.microsoft.com/text/analytics/v2.0'
 
 # GetLyrics
-# Returns list of lyrics for analysis
+# Returns list of lyrics for analysis from given artist
 
 
-def GetLyrics():
-    song_IDs = []
-    songs = {}
+def GetLyrics(artist):
+    songs = []
 
-    # Get list of songs
-    chart_url = '{0}chart.tracks.get?apikey={1}&page=1&page_size=10&\
-    f_has_lyrics=1'.format(MUSIX_BASE, MUSIXMATCH_KEY)
+    # Get tracks from artist
+    tracks_URL = '{0}/track.search?apikey={1}&q_artist={2}&page_size=10&f_has_lyrics=1'\
+                 .format(MUSIX_BASE, MUSIXMATCH_KEY, artist)
 
-    chart_response = requests.get(chart_url)
-    chart_resp_json = chart_response.json()['message']
+    tracks_response = requests.get(tracks_URL)
+    tracks_response.raise_for_status()
+    
+    tracks_json = tracks_response.json()['message']
 
-    if chart_resp_json['header']['status_code'] == 200:
-        print('Got JSON of charts OK')
+    if tracks_json['header']['status_code'] == 200:
+        print('Got tracks OK')
 
-    track_list = chart_resp_json['body']['track_list']
+    tracks = tracks_json['body']['track_list']
+    
+    for i in range(len(tracks)):
+        name = tracks[i]['track']['track_name']
+        code = tracks[i]['track']['track_id']
 
-    for i in range(len(track_list)):
-        song_IDs.append(track_list[i]['track']['track_id'])
-
-    for code in song_IDs:
-        # Get lyrics from songs
-        lyrics_url = '{0}track.lyrics.get?apikey={1}&track_id={2}'\
+        # Get Lyrics for song
+        lyrics_url = '{0}/track.lyrics.get?apikey={1}&track_id={2}'\
                      .format(MUSIX_BASE, MUSIXMATCH_KEY, code)
 
         lyrics_response = requests.get(lyrics_url)
         lyrics_resp_json = lyrics_response.json()['message']
 
         if lyrics_resp_json['header']['status_code'] == 200:
-            print('Got lyrics of song {0} OK'.format(code))
+            print('Got lyrics of song {0} OK'.format(name))
 
         lyrics = lyrics_resp_json['body']['lyrics']['lyrics_body']
 
-        # Get info from songs
-        info_url = '{0}track.get?apikey={1}&track_id={2}'\
-                   .format(MUSIX_BASE, MUSIXMATCH_KEY, code)
+        if not lyrics:
+            lyrics = 'bad' # HACK HACK HACK HACK
 
-        info_response = requests.get(info_url)
-        info_resp_json = info_response.json()['message']
-
-        if info_resp_json['header']['status_code'] == 200:
-            print('Got song info of song {0} OK'.format(code))
-
-        artist = info_resp_json['body']['track']['artist_name']
-        track_name = info_resp_json['body']['track']['track_name']
-
-        # Package song
-        song = Song(artist, track_name, lyrics)
-        songs[code] = song
-        print('Got lyrics and info for {0}: {1}'
-              .format(song.name, song.artist))
-
+        # Package song for analysis
+        song = Song(code, artist, name, lyrics)
+        songs.append(song)
+        
+        
     return songs
+
 
 # Get Emotion
 # Returns emotion of songs entered
@@ -80,24 +72,22 @@ def GetPositiveSongs(songs):
     i = 1
     pos_Songs = []
 
-    for song in songs.itervalues():
+    print('Length of songs: {0}'.format(len(songs)))
+    for song in songs:
         print('Gonna search sentiment for {0}'.format(song.name))
+        lyrics = song.lyrics
 
         # Prepare requests to Text Analytics API
         url = '{0}/sentiment'.format(TEXT_ANALYTICS_BASE)
         headers = {'Ocp-Apim-Subscription-Key': TEXT_ANALYTICS_KEY}
-        json = {'documents': [
-                                {"language": "en",
-                                 "id": str(i),
-                                 "text": song.lyrics
-                                 }
-                            ]
-                }
-
+        j = {'documents':[{"language": "en", "id": str(i), "text": song.lyrics}]}
+        
         # Hit Text Analytics API
-        sentiment_response = requests.post(url, headers=headers, json=json)
-        sentiment_resp_json = sentiment_response.json()['documents']
-        sentiment = sentiment_resp_json[0]['score']
+        sentiment_response = requests.post(url, data=None, headers=headers, json=j, params=None)
+        sentiment_response.raise_for_status()
+        
+        sentiment_json = sentiment_response.json()['documents']
+        sentiment = sentiment_json[0]['score']
 
         if sentiment > .5:
             print('Adding {0}: {1} as a positive track, sentiment: {2}'
@@ -106,15 +96,17 @@ def GetPositiveSongs(songs):
         
         i += 1
     return pos_Songs
-
+    
 
 def main():
-    songs = GetLyrics()
+    artist = raw_input('Please enter an artist to search for: ')
+    songs = GetLyrics(artist)
+    
     pos_Songs = GetPositiveSongs(songs)
-
+    
     good_Song = pos_Songs[0]
     print('Gonna open {0} by {1}'.format(good_Song.name, good_Song.artist))
-    
+
     song_URL = 'https://www.youtube.com/results?search_query={0} {1}'\
                .format(good_Song.artist, good_Song.name)
 
