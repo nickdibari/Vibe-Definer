477f70ddcc509776325b494e98d0c20e14f96307
nickdibari <ndibari@fordham.edu>
2016-12-03 19:58:43 -0500

    Added artist information back to Song class

--- models.py
+++ models.py
@@ -5,7 +5,8 @@
 
 class Song(object):
     """Object to hold Song artist/track name/lyrics"""
-    def __init__(self, code, track_name, lyrics):
+    def __init__(self, code, artist, track_name, lyrics):
         self.code = code
+        self.artist = artist
         self.name = track_name
         self.lyrics = lyrics
