06f18fd371ea5b97a7fc3a920f5e01284bdbb100
nickdibari <ndibari@fordham.edu>
2016-12-08 22:37:33 -0500

    Removed unused code attribute from Song ptII I forgot this last time

--- models.py
+++ models.py
@@ -5,8 +5,7 @@
 
 class Song(object):
     """Object to hold Song artist/track name/lyrics"""
-    def __init__(self, code, artist, track_name, lyrics):
-        self.code = code
+    def __init__(self, artist, track_name, lyrics):
         self.artist = artist
         self.name = track_name
         self.lyrics = lyrics
